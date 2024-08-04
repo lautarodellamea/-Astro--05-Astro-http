02f6d818730ce58e68001e409cff04fad0b052a4
Lautaro <lautarodm98@gmail.com>
2024-08-04 03:31:10 -0300

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,22 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import node from "@astrojs/node";
+// al final use cloudflare por eso desactive node
+// import node from "@astrojs/node";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
-  output: "hybrid",
+  output: "hybrid"
   // en el modo hibrid, mantiene la generacion estatica por defecto a menos que le digamos que sean generado del lado del servidor
-  adapter: node({
-    mode: "standalone"
-  })
+  // adapter: node({
+  //   mode: "standalone"
+  // })
+  ,
+  adapter: cloudflare()
 });
 
 // https://docs.astro.build/en/guides/integrations-guide/node/
